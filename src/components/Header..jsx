fa2b5eb71c1cb0f2912af8536d29f28423cb493e
sadikov16 <sodiqovu880@gmail.com>
2023-06-11 12:10:33 +0500

    alll

---
+++ src/components/Header..jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div>
+        <p>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        </p>
+        <a href="/">ShopNow</a>
+        <select>
+          <option value="">English</option>
+          <option value="">Russian</option>
+          <option value="">Uzbek</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
