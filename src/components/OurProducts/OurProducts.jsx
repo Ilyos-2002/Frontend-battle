c578428a189dcaea71236bc023472de04bd94761
Ilyos Xolmamtov <ilyosxolmamatov3@gmail.com>
2023-06-11 13:15:48 +0500

    cvbnm

--- src/components/OurProducts/OurProducts.jsx
+++ src/components/OurProducts/OurProducts.jsx
@@ -4,6 +4,7 @@ export default function OurProducts() {
     return (
         <div>OurProducts
             <p>dfhjsldkugjreos;djgpoer;dlfjhemo;rdlfjgmerdkf</p>
+            <span>salom</span>
         </div>
     )
 }
